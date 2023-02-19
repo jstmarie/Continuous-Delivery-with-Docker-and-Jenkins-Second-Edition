7a3816107303c748c0aed99235b57a0622dd1f8d
jstmarie <124017474+jstmarie@users.noreply.github.com>
2023-02-19 18:31:45 -0500

    Update Calculator.java
    Added Javadoc.

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,8 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/** This is a Javadoc comment to pass the checkstyle */
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
